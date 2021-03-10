ae8fba8eef5186d98014a413b75efd887f4bf016
github username <github email address>
2021-03-09 20:26:54 -0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails
-  resources :users
+  resources :users do
+    resources :cocktails
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210309235605_create_cocktails.rb
+++ db/migrate/20210309235605_create_cocktails.rb
@@ -4,6 +4,7 @@ class CreateCocktails < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :img_src
       t.string :description
+      t.belongs_to :user
 
       t.timestamps
     end
